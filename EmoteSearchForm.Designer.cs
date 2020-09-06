2ae35c978e9cebbb267d5d7ccf6ad96cb098cb10
Sunaipaa-Kun <skamickrahaman12@gmail.com>
2020-09-06 22:01:27 +0530

    day 1

---
+++ EmoteSearchForm.Designer.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Windows.Forms;
+namespace emote_gui_dotnet_win
+{
+    partial class EmoteSearchForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        TextBox queryInput;
+        ListView emoteList;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+
+            queryInput = new TextBox();
+            emoteList = new ListView();
+
+            //queryInput
+            InitQueryInput();
+            //list
+            InitEmoteList();
+
+            //form
+            AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(660, 230);
+            Controls.Add(emoteList);
+            Controls.Add(queryInput);
+            Opacity = 0.9D;
+            ControlBox = false;
+            ShowIcon = false;
+            ShowInTaskbar = false;
+            StartPosition = FormStartPosition.CenterScreen;
+            FormBorderStyle = FormBorderStyle.FixedToolWindow;
+
+            ResumeLayout(false);
+            PerformLayout();
+
+        }
+
+        private void InitEmoteList()
+        {
+            emoteList.Location = new System.Drawing.Point(12, 12);
+            emoteList.Name = "Emote List";
+            emoteList.Size = new System.Drawing.Size(636, 178);
+            emoteList.TabIndex = 1;
+            emoteList.Columns.Add("Emote", 634);
+            emoteList.UseCompatibleStateImageBehavior = false;
+            emoteList.View = View.Details;
+        }
+
+        private void InitQueryInput()
+        {
+            queryInput.Location = new System.Drawing.Point(12, 196);
+            queryInput.Name = "Query Input";
+            queryInput.Size = new System.Drawing.Size(636, 20);
+            queryInput.TabIndex = 0;
+            queryInput.TextChanged += new System.EventHandler(QueryEmote);
+        }
+
+        #endregion
+    }
+}
+
---
+++ EmoteSearchForm.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Net;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using emote_gui_dotnet_win.DB;
+
+namespace emote_gui_dotnet_win
+{
+    public partial class EmoteSearchForm : Form
+    {
+        private EmoteQueryClient _eqc = new EmoteQueryClient();
+        private WebClient _web = new WebClient();
+        private Queue<string> _imgQ = new Queue<string>();
+
+        public EmoteSearchForm()
+        {
+            InitializeComponent();
+        }
+
+        ~EmoteSearchForm()
+        {
+            _web.Dispose();
+        }
+
+        public void QueryEmote(object sender, EventArgs args)
+        {
+            _imgQ.Clear();
+            emoteList.Items.Clear();
+            emoteList.SmallImageList?.Dispose();
+
+            //var message = "";
+            var dir = @"Z:\Amick\Pictures\Microsoft Clip Organizer";
+            var files = Directory.GetFiles(dir);
+            var imgs = new ImageList(){ ImageSize = new Size(32, 32) };
+
+            emoteList.SmallImageList = imgs;
+
+            // for (int i = 0; i < files.Length; i++)
+            // {
+            //     emoteList.Items.Add(i.ToString(), i);
+            //     //emoteList.Items.Add(item);
+            // }
+            // for (int i = 0; i < files.Length; i++)
+            // {
+            //     string item = files[i];
+            //     emoteList.SmallImageList.Images.Add(Image.FromFile(item));
+            //     //emoteList.Items.Add(item);
+            // }
+
+            FillImages();
+
+            //MessageBox.Show(message);
+
+        }
+
+        private void FillImages()
+        {
+            using (var reader = _eqc.GetEmotesReader(queryInput.Text))
+            {
+                int i = 0;
+                while (reader.Read())
+                {
+                    _imgQ.Enqueue(reader.GetString(0));
+                    emoteList.Items.Add(reader.GetString(1), i++);
+                    //message += $"{reader.GetString(0)} - {reader.GetString(1)}\n";
+                }
+            }
+            //TODO: handle cancellation of this correctly
+        }
+
+        private async void AddImages()
+        {
+            while(_imgQ.Any())
+            {
+                var url = _imgQ.Dequeue();
+                emoteList.SmallImageList.Images.Add(await GetImage(url));
+                emoteList.Refresh();
+            }
+        }
+
+        private async Task<Image> GetImage(string url)
+        {
+            //File.WriteAllBytes( new Uri(url).Segments.Last() + ".png", _web.DownloadData(url));
+            return Image.FromStream(new MemoryStream(await _web.DownloadDataTaskAsync(url)));
+        }
+
+    }
+}
